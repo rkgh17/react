0eed9730ad883049db0c4f1a59551771061e6496
hjh_home <rkgh17@gmail.com>
2023-02-13 18:48:58 +0900

    react study : cleanUp

--- nomad/react-for-beginner-2/src/index.js
+++ nomad/react-for-beginner-2/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Cleanup from "./Cleanup";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <div>
     <App />
-  </React.StrictMode>
+    <Cleanup />
+  </div>
 );
