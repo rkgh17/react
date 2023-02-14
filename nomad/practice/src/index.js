5b8f0ceb10c14b4e48991ecb43bf1c484149bf28
hjh_home <rkgh17@gmail.com>
2023-02-14 19:42:28 +0900

    react study : CoinTracker

--- nomad/practice/src/index.js
+++ nomad/practice/src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import ToDoList from "./ToDoList";
+import CoinTracker from "./CoinTracker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <App />
+    <CoinTracker />
   </div>
 );
