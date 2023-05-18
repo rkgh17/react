a8308a5a0354fc51fb5e776dfbca2f5269c9aa48
Hwang JiHun <82715109+rkgh17@users.noreply.github.com>
2023-05-18 06:41:00 +0000

    카카오 키워드 검색

--- addtest/src/App.js
+++ addtest/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+  // 검색값 관리
+  const [inputSearch, setInputSearch] = useState("");
+  const handleInputSearch = (e) => {
+    setInputSearch(e.target.value);
+  }
+
+  const handleOnKeyPress = (e) => {
+
+    // 검색 수행
+    if(e.key === 'Enter'){
+      // console.log("엔터누름")
+
+      // 카카오 키워드 검색 API axios
+      axios
+        .get("https://dapi.kakao.com/v2/local/search/keyword.json", {
+          params : { query : inputSearch},
+          headers : {Authorization : "KakaoAK bbc0593ca1fbd88db71ccfdd5421ef1e"}
+        }
+        )
+        .then((res) =>{
+          console.log(res)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className=''>
+          <input 
+            placeholder='검색창' 
+            name='inputSearch' 
+            value={inputSearch} 
+            onChange={handleInputSearch}
+            onKeyDown={handleOnKeyPress}></input>
+      </div>
     </div>
   );
 }
